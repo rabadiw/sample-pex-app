d0e030cc55e697e6fc3bee08472cf452eada5988
james.comparin <james.comparin@pexmain.com>
2019-07-15 16:44:43 -0500

    Added files

---
+++ PexSampleAspNet/PexSampleAspNetUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PexSampleAspNetUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PexSampleAspNetUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e6c2118d-a78c-450e-86d5-60f353777309")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PexSampleAspNet/PexSampleAspNetUnitTests/TestHomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PexSampleAspNetUnitTests
+{
+    [TestClass]
+    public class TestHomeController
+    {
+        [TestMethod]
+        public void TestFailure()
+        {
+            Assert.Fail();
+        }
+    }
+}
