97d0959ee363a944d171c466d3fa3bd84e3a0005
Tola Awofolu <oawofolu@pivotal.io>
2019-07-23 15:15:09 -0400

    Temporarily commenting out Config Server related code

--- PexSampleAspNet/Global.asax.cs
+++ PexSampleAspNet/Global.asax.cs
@@ -16,7 +16,7 @@ namespace PexSampleAspNet
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            ApplicationConfig.RegisterConfig("dev");
+            //ApplicationConfig.RegisterConfig("dev");
         }
     }
 }
